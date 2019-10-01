b530e4ec7ef7d4f92dbe44e68d410516501332ba
ylepla <coccinella1995>
2019-10-01 17:56:46 +0200

    add drawer

---
+++ lib/screens/filters_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class FilterScreen extends StatelessWidget {
+  
+  static const routeName = '/filters';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Center(child: Text('Filters'),));
+  }
+}
